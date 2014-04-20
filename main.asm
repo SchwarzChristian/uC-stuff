c49ef7029fec4387df3cfe7f472c32b264d5c7b3
Christian Schwarz <schwarz-chr@web.de>
2014-04-20 13:51:56 +0200

    added first code keyboard API

--- main.asm
+++ main.asm
@@ -1,32 +1,28 @@
 .include "boot.inc"
 .include "lcd.inc"
+.include "kb.inc"
 .include "data.inc"
 
 .cseg
 	
 main:
-	ldi	r16,	0xFF
-	out	DDRB,	r16
-	out	PORTB,	r16
-	
 	rcall	lcd_init
 
-	ldi	ZH,	HIGH(hello) ;say hello
+	ldi	ZH,	HIGH(hello)
 	ldi	ZL,	LOW(hello)
 	rcall	lcd_puts
 
+	rcall	kb_init
+	
 	ldi	r16,	23
-	mov	r0,	r16
-	rcall	lcd_putc
-	mov	r0,	r16
-	rcall	lcd_puth
 
 main_loop:
-	com	r16
-	out	PORTB,	r16
-	rcall	delay_1M
+	rcall	kb_getc
+	rcall	lcd_puth
+	mov	r0,	r16
+	rcall	lcd_putc
 	rjmp	main_loop
-
+	
 	ret			;should never be reached
 	;; end main
 	
